60250d5545dbb57fdf42194ff35b8bf3a1bc0b70
sira <gkrudah@hanyang.ac.kr>
2018-03-25 19:52:50 +0900

    getppid

--- xv6-public/test.c
+++ xv6-public/test.c
@@ -1,13 +1,11 @@
-
 #include "types.h"
 #include "stat.h"
 #include "user.h"
 
 int main()
 {
-	//	perror("failed to fork");
-		printf(1, "My pid is %d\n", getpid());
-		printf(1, "My ppid is %d\n", getppid());
+	printf(1, "My pid is %d\n", getpid());
+	printf(1, "My ppid is %d\n", getppid());
 
 	exit();
 }
